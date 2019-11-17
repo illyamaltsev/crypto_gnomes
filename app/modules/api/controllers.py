c327734d5bd2ce196d65fe74d2b35b7c3a14635c
Illya Maltsev <imaltsev98@gmail.com>
2019-11-17 18:37:57 +0200

    ---

--- app/modules/api/controllers.py
+++ app/modules/api/controllers.py
@@ -1,2 +1,16 @@
+from flask import session, request, Response
+
+from app.database.models import User_Coin, db
 from . import api
 
+
+@api.route('/api/create-wallet/', methods=['POST'])
+def add_wallet():
+    user_id = session.get('user_id')
+
+    coin_id = request.json['value']
+
+    uc = User_Coin(user_id=user_id, coin_id=coin_id, balance=0)
+    db.session.add(uc)
+    db.session.commit()
+    return Response(uc.coin.name, 200)
